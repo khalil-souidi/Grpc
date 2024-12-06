181494378a0b9dbbc5a7bdf85c12cb2494f08b9f
khalil-souidi <khalilsouidi9@gmail.com>
2024-12-06 15:56:49 +0100

    first commit

--- src/main/java/ma/projet/grpc/GrpcApplication.java
+++ src/main/java/ma/projet/grpc/GrpcApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Grpc2Application {
+public class GrpcApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Grpc2Application.class, args);
+        SpringApplication.run(GrpcApplication.class, args);
     }
 
 }
--- src/test/java/ma/projet/grpc/GrpcApplicationTests.java
+++ src/test/java/ma/projet/grpc/GrpcApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Grpc2ApplicationTests {
+class GrpcApplicationTests {
 
     @Test
     void contextLoads() {
